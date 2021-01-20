24c7fbb262b1187be5cead4f9fcb6070b6563df9
Eric Douglass <eric.douglass@gmail.com>
2021-01-19 23:01:47 -0800

    Additional study session setup logic, component/app loading cleanup

--- src/App.js
+++ src/App.js
@@ -87,9 +87,10 @@ const Header = () => {
   const history = useHistory();
 
   useEffect(() => {
-    // Ooh, fun. This fires whenever the app is refreshed, so we can use this to LOAD, I think.
+    // Fires when app 'loads' freshly, so using this space for all loading logic
     const loadedData = load();
-    dispatch({type: actions.LOAD_SAVED_DATA, payload: loadedData})
+    dispatch({type: actions.LOAD_SAVED_DATA, payload: loadedData});
+    history.push('/');
   }, [dispatch]);
 
   return (
@@ -399,38 +400,98 @@ const DeckPreview = (props) => {
 const SessionSetupComponent = () => {
   const [state, dispatch] = useContext(Context);
   const [sessionPrep, setSessionPrep] = useState({
-    decksToChoose: [state.decks],
-    decksToUse: []
+    decksToChoose: state.decks,
+    decksToUse: [],
+    sessionEndCondition: undefined,
+    sessionEndNumber: 5
   })
   const history = useHistory();
 
   function addDeckToSession(deck) {
-    // THIS FXN: 
-    setSessionPrep({... sessionPrep, decksToUse: [... sessionPrep.decksToUse, deck]});
+    // THIS FXN: Take deck from Choose to Use, adjusting both
+    let toChoose = sessionPrep.decksToChoose.filter(oldDeck => oldDeck.id !== deck.id);
+    let toUse = [...sessionPrep.decksToUse];
+    toUse.push(deck);
+    setSessionPrep({... sessionPrep, decksToChoose: toChoose, decksToUse: toUse});
+  }
+
+  function removeDeckFromSession(deck) {
+    let toUse = sessionPrep.decksToUse.filter(oldDeck => oldDeck.id !== deck.id);
+    let toChoose = [...sessionPrep.decksToChoose];
+    toChoose.push(deck);
+    setSessionPrep({... sessionPrep, decksToChoose: toChoose, decksToUse: toUse});
   }
 
   /*
     For this component...
-    -- want to be able to select deck(s) to study from
-    -- select details, like "once through" or "unlimited iterations"
+    -- select session details/plan, like "once through" or "unlimited iterations"
     -- pass this all to SessionStudy to begin the session
-    -- 
   */
-
-  // ERROR: This page works totally fine UNLESS I reload the page, then all the decks vanish into thin air. Whoopsie.
-
-  useEffect(() => {
-    // HERE: Let's make a new local state variable that's all the decks we have so we can "move" them from ToChoose to ToUse
-    console.log(`Initializing session prep decks to choose from...`);
-    setSessionPrep({...sessionPrep, decksToChoose: [...state.decks]});
-  }, []);
 
   return (
     <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
       <h1>Session Setup</h1>
+      
+      {/* HERE: Menu for session deets  */}
+      <div id='session-details-menu'>
+        
+        
+        <h2>For this session, we'll keep going through the deck(s) until...</h2>
+        <div style={{display: 'flex', flexDirection: 'row', width: '80vw'}}>
+
+          <div style={{display: 'flex', flex: '2', flexDirection: 'row', justifyContent: 'space-around'}}>
+            <button className='btn small-btn' onClick={() => setSessionPrep({...sessionPrep, sessionEndCondition: 'time'})}>we reach a time limit</button>
+            <button className='btn small-btn' onClick={() => setSessionPrep({...sessionPrep, sessionEndCondition: 'iterations'})}>we go through all cards X times</button>
+            <button className='btn small-btn' onClick={() => setSessionPrep({...sessionPrep, sessionEndCondition: 'user'})}>we choose to end the session</button>
+          </div>
+
+          <div style={{flex: '1'}}>
+            {sessionPrep.sessionEndCondition === 'time' &&
+            <div>
+              <h3>Set Time Limit</h3>
+              <select value={sessionPrep.sessionEndNumber} onChange={e => setSessionPrep({...sessionPrep, sessionEndNumber: e.target.value})}>
+                <option value={5}>5 min</option>
+                <option value={10}>10 min</option>
+                <option value={15}>15 min</option>
+                <option value={20}>20 min</option>
+                <option value={25}>25 min</option>
+                <option value={30}>30 min</option>
+                <option value={35}>35 min</option>
+                <option value={40}>40 min</option>
+                <option value={45}>45 min</option>
+              </select>
+            </div>
+            }
+            {sessionPrep.sessionEndCondition === 'iterations' &&
+            <div>
+              <h3>Set Iterations Limit</h3>
+              <input type='number' value={sessionPrep.sessionEndNumber} min={1} max={100} onChange={e => setSessionPrep({...sessionPrep, sessionEndNumber: e.target.value})}></input>
+            </div>
+            }
+            {sessionPrep.sessionEndCondition === 'user' &&
+            <div>
+              <h3>You Good</h3>
+            </div>
+            }
+          </div>
+
+        </div>
+
+      </div>
+
       <h2>Select Decks to Study With</h2>
-      {sessionPrep.decksToChoose.map((deck, index) => (<button key={index} className='btn' onClick={() => addDeckToSession(deck)}>{deck.name}</button>))}
-      <button className='btn' onClick={() => history.push('/session_study')}>GO STUDY</button>
+      <div id='session-cards-choose-menu'>
+        <div id='decks-to-choose' className='session-setup-decks'>
+          {sessionPrep.decksToChoose.map((deck, index) => (<button key={index} className='btn' onClick={() => addDeckToSession(deck)}>{deck.name}</button>))}
+        </div>
+
+        <div id='decks-to-use' className='session-setup-decks'>
+          {sessionPrep.decksToUse.map((deck, index) => (<button key={index} className='btn' onClick={() => removeDeckFromSession(deck)}>{deck.name}</button>))}
+        </div>
+      </div>
+
+      <button className='btn' onClick={() => history.push('/session_study')}>{sessionPrep.decksToUse.length > 0 ? 'READY TO STUDY!' : 'Choose 1+ Deck(s)'}</button>
+
     </div>
   )
 }
