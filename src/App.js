9c7f5f366dbf342c220c5654d702686ac4ebdb5c
Eric Douglass <eric.douglass@gmail.com>
2021-01-23 14:45:06 -0800

    Added card search and filter options

--- src/App.js
+++ src/App.js
@@ -370,8 +370,14 @@ const ModifyDeckComponent = () => {
 
 const ViewCardsComponent = () => {
   const [state, dispatch] = useContext(Context);
-  // HERE: local state object for filters
-  // HERE: local state for search string
+  const [filter, setFilter] = useState({
+    front: true,
+    back: true,
+    categories: true,
+    description: true
+  });
+  const [search, setSearch] = useState('');
+  const [viewedCards, setViewedCards] = useState([]);
 
   useEffect(() => {
     dispatch({type: actions.UPDATE_WHATDO, payload: {page: '/view_cards', currentAction: {}}});
@@ -379,13 +385,56 @@ const ViewCardsComponent = () => {
 
   useEffect(() => {
     save(state);
+    setViewedCards(state.cards);
   }, [state]);
 
+  useEffect(() => {
+    if (search === '') {
+      setViewedCards(state.cards);
+    } else {
+      setViewedCards(state.cards.filter(card => {
+        let searchFound = false;
+        if (filter.front) {
+          searchFound = card.front.includes(search);
+          if (searchFound) return card;
+        }
+        if (filter.back) {
+          searchFound = card.back.includes(search);
+          if (searchFound) return card;
+        }
+        if (filter.categories) {
+          searchFound = card.categories.includes(search);
+          if (searchFound) return card;
+        }
+        if (filter.description) {
+          searchFound = card.description?.includes(search);
+          if (searchFound) return card;
+        }
+        return searchFound;
+      }));
+  }
+  }, [search])
+
   return (
-    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
-      <h1>Behold! Your CARDS!</h1>
+    <div className='flex-centered flex-col'>
+      <div className='flex-centered flex-row' style={{width: '90vw', justifyContent: 'space-around', border: '1px solid black'}}>
+        <p>Search and Mod Cards</p>
+        <div>
+          <label>Search: </label>
+          <input style={{height: '40px', fontSize: '30px'}} type='text' value={search} onChange={e => setSearch(e.target.value)}></input>
+        </div>
+
+        <div>
+          <label>Search Areas: </label>
+          <button className='btn small-btn' style={{backgroundColor: filter.front ? 'green' : 'red'}} onClick={() => setFilter({...filter, front: !filter.front})} >Front</button>
+          <button className='btn small-btn' style={{backgroundColor: filter.back ? 'green' : 'red'}} onClick={() => setFilter({...filter, back: !filter.back})}   >Back</button>
+          <button className='btn small-btn' style={{backgroundColor: filter.categories ? 'green' : 'red'}} onClick={() => setFilter({...filter, categories: !filter.categories})}   >Categories</button>
+          <button className='btn small-btn' style={{backgroundColor: filter.description ? 'green' : 'red'}} onClick={() => setFilter({...filter, description: !filter.description})}  >Description</button>
+        </div>
+        
+      </div>
       <div className='cards-list-holder'>
-        {state.cards.map((card, index) => (
+        {viewedCards.map((card, index) => (
         <CardPreview card={card} key={index} />
         ))}
       </div>
@@ -395,10 +444,19 @@ const ViewCardsComponent = () => {
 
 const CardPreview = (props) => {
   const {card} = props;
+  const [optionsVisible, setOptionsVisible] = useState(false);
 
   return (
-    <div className='card-preview'>
-      <p style={{fontSize: '24px', fontWeight: '700'}}>{card.front}</p>
+    <div className='card-preview' onMouseEnter={() => setOptionsVisible(true)} onMouseLeave={() => setOptionsVisible(false)}>
+      <div style={{flex: 3}}>
+        <p style={{fontSize: '24px', fontWeight: '700'}}>{card.front}</p>
+      </div>
+      
+      <div style={{visibility: optionsVisible ? 'visible' : 'hidden', flex: 1}}>
+        <button>Peep</button>
+        <button>Delete</button>
+      </div>
+
     </div>
   )
 }
